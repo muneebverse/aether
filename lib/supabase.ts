5e8cc6ea4d2b8ac0b2be871bba1d6d1798612f3a
Muneeb Sultan <itsmuneeb825@gmail.com>
2026-07-12 12:07:13 -0700

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,11 +1,2 @@
-import { PrismaClient } from '@prisma/client';
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const db =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
+// This file is unused but kept for backwards compatibility
+export {};
