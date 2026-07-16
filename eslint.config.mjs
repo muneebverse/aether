f78933eca15c3a962e26a985e6e11565bf39c0cb
Muneeb Sultan <itsmuneeb825@gmail.com>
2026-07-16 15:13:54 +0500

    Remove TypeScript ESLint plugin from config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,10 @@
 import nextPlugin from 'eslint-plugin-next';
-import typescriptPlugin from '@typescript-eslint/eslint-plugin';
 
 export default [
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     plugins: {
       '@next/next': nextPlugin,
-      '@typescript-eslint': typescriptPlugin,
     },
     rules: {
       'react/no-unescaped-entities': 'off',
